b256eea4753a14f0fcedc2c939d05f067876efee
Константин <konstkonst55@mail.ru>
2026-05-14 00:50:08 +0700

    add models

---
+++ ChessValidator.Shared/Models/ChessBoard.cs
@@ -0,0 +1,24 @@
+﻿using ChessValidator.Shared.Pieces;
+
+namespace ChessValidator.Shared.Models
+{
+    public class ChessBoard
+    {
+        private readonly List<Piece> _pieces = new();
+
+        public IReadOnlyCollection<Piece> Pieces => _pieces;
+
+        public void AddPiece(Piece piece)
+        {
+            if (_pieces.Any(p => p.Position.X == piece.Position.X && p.Position.Y == piece.Position.Y))
+                throw new Exceptions.DuplicatePositionException();
+
+            _pieces.Add(piece);
+        }
+
+        public Piece? GetPieceAt(Position position)
+        {
+            return _pieces.FirstOrDefault(p => p.Position.X == position.X && p.Position.Y == position.Y);
+        }
+    }
+}
---
+++ ChessValidator.Shared/Models/Position.cs
@@ -0,0 +1,19 @@
+﻿using ChessValidator.Shared.Exceptions;
+
+namespace ChessValidator.Shared.Models
+{
+    public class Position
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Position(int x, int y)
+        {
+            if (x < 0 || x > 7 || y < 0 || y > 7)
+                throw new InvalidPositionException();
+
+            X = x;
+            Y = y;
+        }
+    }
+}
