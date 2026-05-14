e728f514f34d3f6021618e34df18cc603322f617
Константин <konstkonst55@mail.ru>
2026-05-14 13:04:56 +0700

    add grid for optimization

--- ChessValidator.Shared/Models/ChessBoard.cs
+++ ChessValidator.Shared/Models/ChessBoard.cs
@@ -5,20 +5,23 @@ namespace ChessValidator.Shared.Models
     public class ChessBoard
     {
         private readonly List<Piece> _pieces = new();
+        private readonly Piece?[,] _grid = new Piece?[8, 8];
 
         public IReadOnlyCollection<Piece> Pieces => _pieces;
 
         public void AddPiece(Piece piece)
         {
-            if (_pieces.Any(p => p.Position.X == piece.Position.X && p.Position.Y == piece.Position.Y))
+            if (_grid[piece.Position.X, piece.Position.Y] != null)
+            {
                 throw new Exceptions.DuplicatePositionException();
+            }
 
             _pieces.Add(piece);
+            _grid[piece.Position.X, piece.Position.Y] = piece;
         }
 
-        public Piece? GetPieceAt(Position position)
-        {
-            return _pieces.FirstOrDefault(p => p.Position.X == position.X && p.Position.Y == position.Y);
-        }
+        public Piece? GetPieceAt(Position position) => _grid[position.X, position.Y];
+
+        public bool IsSquareOccupied(int x, int y) => _grid[x, y] != null;
     }
 }
