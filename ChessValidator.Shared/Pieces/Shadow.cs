dd1065b4ab7baf5a7a075b000d570ce8f6504b09
Константин <konstkonst55@mail.ru>
2026-05-14 13:08:23 +0700

    implement shadow piece

---
+++ ChessValidator.Shared/Pieces/Shadow.cs
@@ -0,0 +1,15 @@
+﻿using ChessValidator.Shared.Models;
+
+namespace ChessValidator.Shared.Pieces
+{
+    public class Shadow : Piece
+    {
+        public Shadow(Position position) : base(position) { }
+        public override string Symbol => "S";
+
+        public override bool CanAttack(Position target, ChessBoard board)
+        {
+            return IsOrthogonalAttack(target, board) || IsDiagonalAttack(target, board);
+        }
+    }
+}
