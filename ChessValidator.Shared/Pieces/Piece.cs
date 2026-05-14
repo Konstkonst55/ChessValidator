2924989e19724391c3c52bbbffcb42a62af0e470
Константин <konstkonst55@mail.ru>
2026-05-14 13:07:31 +0700

    remove position allocation in while

--- ChessValidator.Shared/Pieces/Piece.cs
+++ ChessValidator.Shared/Pieces/Piece.cs
@@ -5,17 +5,25 @@ namespace ChessValidator.Shared.Pieces
     public abstract class Piece
     {
         public Position Position { get; }
+
         protected Piece(Position position) => Position = position;
 
         public abstract bool CanAttack(Position target, ChessBoard board);
+
         public abstract string Symbol { get; }
 
         protected bool IsOrthogonalAttack(Position target, ChessBoard board)
         {
             if (Position.X == target.X)
+            {
                 return IsPathClear(target, board, 0, target.Y > Position.Y ? 1 : -1);
+            }
+
             if (Position.Y == target.Y)
+            {
                 return IsPathClear(target, board, target.X > Position.X ? 1 : -1, 0);
+            }
+
             return false;
         }
 
@@ -25,8 +33,10 @@ namespace ChessValidator.Shared.Pieces
             {
                 int dx = target.X > Position.X ? 1 : -1;
                 int dy = target.Y > Position.Y ? 1 : -1;
+             
                 return IsPathClear(target, board, dx, dy);
             }
+
             return false;
         }
 
@@ -34,11 +44,18 @@ namespace ChessValidator.Shared.Pieces
         {
             int x = Position.X + dx;
             int y = Position.Y + dy;
+
             while (x != target.X || y != target.Y)
             {
-                if (board.GetPieceAt(new Position(x, y)) != null) return false;
-                x += dx; y += dy;
+                if (board.IsSquareOccupied(x, y))
+                {
+                    return false;
+                }
+
+                x += dx;
+                y += dy;
             }
+
             return true;
         }
     }
