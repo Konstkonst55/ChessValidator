69a0beedd023e48c175d18c4c48fdf191e4922b1
Константин <konstkonst55@mail.ru>
2026-05-14 13:09:21 +0700

    add attack detector service

---
+++ ChessValidator.Shared/Services/Logic/AttackDetector.cs
@@ -0,0 +1,28 @@
+﻿using ChessValidator.Shared.Models;
+
+namespace ChessValidator.Shared.Services.Logic
+{
+    public class AttackDetector : IAttackDetector
+    {
+        public List<string> Detect(ChessBoard board)
+        {
+            var result = new List<string>();
+
+            foreach (var attacker in board.Pieces)
+            {
+                foreach (var target in board.Pieces)
+                {
+                    if (attacker == target)
+                        continue;
+
+                    if (attacker.CanAttack(target.Position, board))
+                    {
+                        result.Add($"{attacker.GetType().Name} ({attacker.Position.X},{attacker.Position.Y}) -> {target.GetType().Name} ({target.Position.X},{target.Position.Y})");
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ChessValidator.Shared/Services/Logic/IAttackDetector.cs
@@ -0,0 +1,9 @@
+﻿using ChessValidator.Shared.Models;
+
+namespace ChessValidator.Shared.Services.Logic
+{
+    public interface IAttackDetector
+    {
+        public List<string> Detect(ChessBoard board);
+    }
+}
